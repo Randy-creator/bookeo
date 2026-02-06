01cad6142516b2ef01bd05fdcf9502add478625b
Randy-creator <hei.randy.2@gmail.com>
2026-02-06 18:16:34 +0600

    chore: init project

---
+++ src/main/java/com/bookeo/bookeo/BookeoApplication.java
@@ -0,0 +1,13 @@
+package com.bookeo.bookeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookeoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bookeo/bookeo/endpoint/rest/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.bookeo.bookeo.endpoint.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
